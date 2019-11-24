fd46c15e6f49ac91de7bf69e47dbff0c842e43c5
maniknath <maniknath79@gmail.co>
2019-11-24 15:41:43 -0500

    added model and views

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using FinalProject.Models;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
 
 namespace FinalProject.Controllers
 {
@@ -21,6 +20,22 @@ namespace FinalProject.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Search(string Id)
+        {
+            string key = "200641663-d6ba0e012de562cebaf18e1d1874a93f";
+            HttpWebRequest request = WebRequest.CreateHttp($"https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&key={Id}/{key}.json");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream s = response.GetResponseStream();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APItext = rd.ReadToEnd();
+            JToken t = JToken.Parse(APItext);
+            Trails d = new Trails(t);
+            ViewBag.Id = Id;
+            return View(d);
         }
 
         public IActionResult Privacy()
