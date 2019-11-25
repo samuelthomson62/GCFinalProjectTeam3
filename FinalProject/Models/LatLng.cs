47fb800b8990b13a3d76683a04c8968e87d17cc1
Sam Thomson <samuelthomson62@gmail.com>
2019-11-25 15:26:42 -0500

    added MapQuest API via LatLng model and TrailDAL

---
+++ FinalProject/Models/LatLng.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class LatLng
+    {
+        public string Lat { get; set; }
+        public string Lng { get; set; }
+        public LatLng()
+        {
+
+        }
+
+        public LatLng(JToken j)
+        {
+            this.Lat = j["results"][0]["locations"][0]["latLng"]["lat"].ToString();
+            this.Lng = j["results"][0]["locations"][0]["latLng"]["lng"].ToString();
+        }
+    }
+}
---
+++ FinalProject/Models/TrailDAL.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class TrailDAL
+    {
+
+
+        public static LatLng GetLatLon(string location)
+        {
+            // this method has been tested and works -Sam <3
+
+            List<double> LatLon = new List<double>();
+
+            HttpWebRequest request = WebRequest.CreateHttp($"http://www.mapquestapi.com/geocoding/v1/address?key=RAeJl9of0cOtFhtd9VtoGGI8jUAvaG4l&location={location}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APItext = rd.ReadToEnd();
+            JToken t = JToken.Parse(APItext);
+            LatLng l = new LatLng(t);
+            return l;
+        }
+
+    }
+}
