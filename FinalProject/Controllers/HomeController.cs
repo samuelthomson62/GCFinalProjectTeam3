f9b9a31724780636186c6f5e4f6d0dfd6981cf67
LizHitches <liz.hitches@gmail.com>
2019-12-18 19:14:15 -0500

    css stuff and organization

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -16,464 +16,53 @@ namespace FinalProject.Controllers
 {
     public class HomeController : Controller
     {
+     
         private readonly ApplicationDbContext _db;
         public HomeController(ApplicationDbContext db)
         {
             _db = db;
         }
+       // BucketListController bucketListController = new BucketListController();
 
-        //private readonly ILogger<HomeController> _logger;
-
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
+     
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Recomendations()
-        {
-            return View();
-        }
         public IActionResult GeneralSearch(string state)
         {
             List<Trails> trail = TrailDAL.GetResults(state);
             return View(trail);
         }
-        public string GetBuild()
-        {
-
-            var bodybuild = from n in _db.UserLevel
-                            where n.UserName == User.Identity.Name
-                            select n.BodyBuild;
-
-            return bodybuild.Single();
-        }
-        public int GetTimes()
-        {
-            var Times = from n in _db.UserLevel
-                        where n.UserName == User.Identity.Name
-                        select n.TimesDoneBefore;
-
-
-            return Times.Single();
-        }
-        public string GetPreExisitingCondition()
-        {
-
-            var condition = from n in _db.UserLevel
-                            where n.UserName == User.Identity.Name
-                            select n.PreExistingCondition;
-
-
-            return condition.Single();
-        }
-        public string GetState()
-        {
-            var state = from n in _db.UserLevel
-                        where n.UserName == User.Identity.Name
-                        select n.State;
-            return state.Single();
-        }
-        public string UserLevel()
-        {
-            var PreExistingCondition = GetPreExisitingCondition();
-            var BodyBuild = GetBuild();
-            var TimesDoneBefore = 5;
-            string difficulty = "";
-            //-----------------------------------No preexisting Condition--------------------------------------------
-            if (PreExistingCondition == "n")
-            {
-
-                // Plump____________________________________________________
-                if (BodyBuild == "plump")
-                {
-                    if (TimesDoneBefore <= 3)
-                    {
-                        difficulty = "green";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 4 && TimesDoneBefore <= 6)
-                    {
-                        difficulty = "greenBlue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 7 && TimesDoneBefore <= 8)
-                    {
-                        difficulty = "greenBlue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 9 && TimesDoneBefore <= 10)
-                    {
-                        difficulty = "greenBlue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 11 && TimesDoneBefore <= 15)
-                    {
-                        difficulty = "blue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 15)
-                    {
-                        difficulty = "blueBlack";
-                        return difficulty;
-                    }
-
-                }
-                // Average____________________________________________________
-                if (BodyBuild == "average")
-                {
-                    if (TimesDoneBefore <= 3)
-                    {
-                        difficulty = "green";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 4 && TimesDoneBefore <= 6)
-                    {
-                        difficulty = "greenBlue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 7 && TimesDoneBefore <= 8)
-                    {
-                        difficulty = "greenBlue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 9 && TimesDoneBefore <= 10)
-                    {
-                        difficulty = "blue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 11)
-                    {
-                        difficulty = "black";
-                        return difficulty;
-                    }
-
-                }
-                // Athletic____________________________________________________
-
-                if (BodyBuild == "athletic")
-                {
-                    if (TimesDoneBefore == 0)
-                    {
-                        difficulty = "green";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 1 && TimesDoneBefore <= 3)
-                    {
-                        difficulty = "greenBlue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 4 && TimesDoneBefore <= 6)
-                    {
-                        difficulty = "blue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 7 && TimesDoneBefore <= 10)
-                    {
-                        difficulty = "blueBlack";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 11 && TimesDoneBefore <= 15)
-                    {
-                        difficulty = "black";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 15)
-                    {
-                        difficulty = "dBlack";
-                        return difficulty;
-                    }
-
-                }
-            }
-            //-----------------------------------with preexisting Condition--------------------------------------------
-            if (PreExistingCondition == "y")
-            {
-                // Plump____________________________________________________
-                if (BodyBuild == "plump")
-                {
-                    if (TimesDoneBefore <= 6)
-                    {
-                        difficulty = "green";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 7 && TimesDoneBefore <= 10)
-                    {
-                        difficulty = "greenBlue";
-                        return difficulty;
-                    }
-
-                    if (TimesDoneBefore >= 11)
-                    {
-                        difficulty = "blue";
-                        return difficulty;
-                    }
-                }
-                // Average____________________________________________________
-                if (BodyBuild == "average")
-                {
-                    if (TimesDoneBefore <= 3)
-                    {
-                        difficulty = "green";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 4 && TimesDoneBefore <= 6)
-                    {
-                        difficulty = "greenBlue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 7 && TimesDoneBefore <= 8)
-                    {
-                        difficulty = "blue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 9 && TimesDoneBefore <= 10)
-                    {
-                        difficulty = "blue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 11)
-                    {
-                        difficulty = "blueBlack";
-                        return difficulty;
-                    }
-                }
-                // Athletic____________________________________________________
-                if (BodyBuild == "athletic")
-                {
-                    if (TimesDoneBefore == 0)
-                    {
-                        difficulty = "green";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 1 && TimesDoneBefore <= 3)
-                    {
-                        difficulty = "greenBlue";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 4 && TimesDoneBefore <= 10)
-                    {
-                        difficulty = "blue";
-                        return difficulty;
-                    }
-
-                    if (TimesDoneBefore >= 11 && TimesDoneBefore <= 15)
-                    {
-                        difficulty = "blueBlack";
-                        return difficulty;
-                    }
-                    if (TimesDoneBefore >= 15)
-                    {
-                        difficulty = "dBlack";
-                        return difficulty;
-                    }
-                }
-            }
-            return difficulty;
-        }
+        
         public IActionResult TrailsDetail(int Id)
         {
-            //We have to call the API again to get the trail we want to save.
             Trails x = TrailDAL.GetTrailById(Id);
+            List<Forcast> f = TrailDAL.OpenWeatherGetForcast(x.Location);
+            ViewBag.Forcast = f;
+            ViewBag.Water = x.Length % 6;
             return View(x);
         }
 
-        public IActionResult AddCheckMark(int? id)
-            {
-            var completeMark = "https://solidwize.com/wp-content/uploads/2012/04/Green-Check-Mark.jpg";
-            var checkmark =
-                from n in _db.Trails
-                where n.Id == id
-                select n;
-
-            foreach (Trails bl in checkmark)
-            {
-                if (completeMark== bl.CompleteMark)
-                {
-                    //TODO: Add a view that says is already completed if you want to add this 
-                    return RedirectToAction(nameof(AlreadyMarkedCompleted));
-                }
-
-                else
-                {
-                    bl.CompleteMark = completeMark;
-                    var user = from n in _db.UserLevel
-                               where n.UserName == User.Identity.Name
-                               select n;
-
-                    foreach (ApplicationUser u in user)
-                    {
-                        int current = u.TimesDoneBefore;
-                        u.TimesDoneBefore = current + 1;
-                        // Insert any additional changes to column values.
-                    }
-                    try
-                    {
-                        _db.SaveChanges();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                        // Provide for exceptions.
-                    }
-                }
-            }
-
-            try
-            { _db.SaveChanges(); }
-            catch (Exception e)
-            { Console.WriteLine(e);}    
-
-            _db.SaveChanges();
-             return  RedirectToAction(nameof(BucketList));
-            }
-
-        public IActionResult AlreadyMarkedCompleted()
+        
+        public IActionResult Privacy()
         {
             return View();
         }
-        public IActionResult MarkIncomplete(int? id)
-        {
-            var incompleteMark = "https://www.trzcacak.rs/myfile/detail/51-515377_x-mark-transparent-background-png-x.png";
-
-
-            var checkmark =
-                from n in _db.Trails
-                where n.Id == id
-                select n;
-
-            foreach (Trails bl in checkmark)
-            {
-                if (incompleteMark==bl.CompleteMark)
-                {
-                    return RedirectToAction(nameof(BucketList));
-
-                }
-                else
-                {
-                    bl.CompleteMark = incompleteMark;
-                    var user = from n in _db.UserLevel
-                               where n.UserName == User.Identity.Name
-                               select n;
-
-                    foreach (ApplicationUser u in user)
-                    {
-                        int current = u.TimesDoneBefore;
-                        u.TimesDoneBefore = current - 1;
-                    }
-                    try
-                    {
-                        _db.SaveChanges();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                    }
-
-
-                }
-            }
-
-            try
-            {
-                _db.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-           
-            _db.SaveChanges();
-
-            return RedirectToAction(nameof(BucketList));
-        }
-
-        [Authorize]
-        [HttpPost]
-        public IActionResult AddToBucketList( string name, string location, string summary, string image, decimal length, string date)
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                string status = "https://www.trzcacak.rs/myfile/detail/51-515377_x-mark-transparent-background-png-x.png";
-                string id = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-                var input = new Trails { UserId = id, Location = location, Name = name, Summary = summary, ImgSmallMed = image, Length = length, Date=date, CompleteMark=status};
-
-
-                _db.Add(input);
-                _db.SaveChanges();
-
-                return RedirectToAction(nameof(Confirm));
-            }
-            else
-            {
-                return RedirectToAction("./Identity/Account/Login");
-
-            }
-        }
-
-        public IActionResult Confirm()
+        public IActionResult Essentials()
         {
             return View();
         }
-             
-        public async Task<IActionResult> BucketList()
-        {
-            return View(await _db.Trails.ToListAsync());
-        }
-      [Authorize]
-        public IActionResult Search()
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                string Difficulty = UserLevel();
-                string state = GetState();
 
-                List<Trails> trail = TrailDAL.GetResults(state, Difficulty);
-                return View(trail);
-            }
-
-            else
-            {
-                return RedirectToAction("./Identity/Account/Login");
-            }
-        }
-
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var trails = await _db.Trails
-                .FirstOrDefaultAsync(t => t.Id == id);
-            if (trails == null)
-            {
-                return NotFound();
-            }
-
-            return View(trails);
-        }
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var trails = await _db.Trails.FindAsync(id);
-            _db.Trails.Remove(trails);
-            await _db.SaveChangesAsync();
-            return RedirectToAction(nameof(BucketList));
-        }
-        public IActionResult BucketListDateModal()  
+        public IActionResult Contact()
         {
             return View();
         }
-       public IActionResult Privacy()
-        {
-            return View();
-        }
+
+
+
+
         private bool TrailsExists(int id)
         {
             return _db.Trails.Any(e => e.Id == id);
--- FinalProject/Models/TrailDAL.cs
+++ FinalProject/Models/TrailDAL.cs
@@ -10,14 +10,11 @@ namespace FinalProject.Models
 {
     public class TrailDAL
     {
-
-
         public static LatLng GetLatLng(string location)
         {
             //this method uses the MapQuest API to convert a string location like "Grand Rapids, MI" to Latitude and Longitude for the Hiking API
             // this method has been tested and works -Sam <3
-            
-            HttpWebRequest request = WebRequest.CreateHttp($"http://www.mapquestapi.com/geocoding/v1/address?key=RAeJl9of0cOtFhtd9VtoGGI8jUAvaG4l&location={location}");
+            HttpWebRequest request = WebRequest.CreateHttp($"http://www.mapquestapi.com/geocoding/v1/address?key={Secret.APIkey1}&location={location}");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             StreamReader rd = new StreamReader(response.GetResponseStream());
             string APItext = rd.ReadToEnd();
@@ -29,7 +26,7 @@ namespace FinalProject.Models
         {
             // this method runs the Hiking API and returns a list of Trails
             LatLng l = GetLatLng(location);
-            HttpWebRequest request = WebRequest.CreateHttp($"https://www.hikingproject.com/data/get-trails?lat={l.Lat}&lon={l.Lng}&maxDistance=200&maxResult=100&key=200641663-d6ba0e012de562cebaf18e1d1874a93f");
+            HttpWebRequest request = WebRequest.CreateHttp($"https://www.hikingproject.com/data/get-trails?lat={l.Lat}&lon={l.Lng}&maxDistance=200&maxResult=100&key={Secret.APIkey2}");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             StreamReader rd = new StreamReader(response.GetResponseStream());
             string APItext = rd.ReadToEnd();
@@ -48,7 +45,7 @@ namespace FinalProject.Models
         public static List<Trails> GetResults(string location, string difficulty)
         {
             LatLng l = GetLatLng(location);
-            HttpWebRequest request = WebRequest.CreateHttp($"https://www.hikingproject.com/data/get-trails?lat={l.Lat}&lon={l.Lng}&maxDistance=200&maxResult=100&key=200641663-d6ba0e012de562cebaf18e1d1874a93f");
+            HttpWebRequest request = WebRequest.CreateHttp($"https://www.hikingproject.com/data/get-trails?lat={l.Lat}&lon={l.Lng}&maxDistance=200&maxResult=100&key={Secret.APIkey2}");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             StreamReader rd = new StreamReader(response.GetResponseStream());
             string APItext = rd.ReadToEnd();
@@ -58,7 +55,7 @@ namespace FinalProject.Models
             List<JToken> x = t["trails"].ToList();
             foreach (JToken y in x)
             {
-                if(y["difficulty"].ToString() == difficulty)
+                if (y["difficulty"].ToString() == difficulty)
                 {
                     Trails z = new Trails(y);
                     Result.Add(z);
@@ -69,7 +66,7 @@ namespace FinalProject.Models
         //Created this method below to call for TrailsDetail -Sammyboy <3
         public static Trails GetTrailById(int Id)
         {
-            HttpWebRequest request = WebRequest.CreateHttp($"https://www.hikingproject.com/data/get-trails-by-id?ids={Id}&key=200641663-d6ba0e012de562cebaf18e1d1874a93f");
+            HttpWebRequest request = WebRequest.CreateHttp($"https://www.hikingproject.com/data/get-trails-by-id?ids={Id}&key={Secret.APIkey2}");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             StreamReader rd = new StreamReader(response.GetResponseStream());
             string APItext = rd.ReadToEnd();
@@ -79,8 +76,42 @@ namespace FinalProject.Models
             //I have to grab the first object of that list to get the Trails model to convert the properties propperly
             List<JToken> x = t["trails"].ToList();
             Trails myTrail = new Trails(x[0]);
-
             return myTrail;
+        }
+        public static List<Forcast> OpenWeatherGetForcast(string Location)
+        {
+            LatLng l = GetLatLng(Location);
+            HttpWebRequest request = WebRequest.CreateHttp($"http://api.openweathermap.org/data/2.5/forecast?lat={l.Lat}&lon={l.Lng}&units=imperial&APPID={Secret.OpenKey}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APItext = rd.ReadToEnd();
+            JToken t = JToken.Parse(APItext);
+
+            List<Forcast> fullList = new List<Forcast>();
+            List<JToken> x = t["list"].ToList();
+            foreach (JToken token in x)
+            {
+                Forcast y = new Forcast(token);
+                fullList.Add(y);
+            }
+            List<Forcast> fiveDays = new List<Forcast>();
+            fiveDays.Add(fullList[0]);
+            fiveDays.Add(fullList[7]);
+            fiveDays.Add(fullList[15]);
+            fiveDays.Add(fullList[23]);
+            fiveDays.Add(fullList[31]);
+            return fiveDays;
+        }
+        public static Forcast OpenWeatherCurrentForcast(string Location)
+        {
+            LatLng l = GetLatLng(Location);
+            HttpWebRequest request = WebRequest.CreateHttp($"http://api.openweathermap.org/data/2.5/weather?lat={l.Lat}&lon={l.Lng}&units=imperial&APPID={Secret.OpenKey}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APItext = rd.ReadToEnd();
+            JToken t = JToken.Parse(APItext);
+            Forcast f = new Forcast(t);
+            return f;
         }
     }
 }
